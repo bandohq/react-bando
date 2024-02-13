338cb02933f5d231becf783c128c46b8c2c3db3d
luisgj <guayabas@hey.com>
2024-02-12 18:56:10 -0600

    styled link and new colors

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,15 +2,13 @@ import Box, { BoxProps } from '@mui/material/Box';
 import { Grid, Link, Typography, Container, IconButton } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import XIcon from '@mui/icons-material/X';
-import Logo from '../../assets/logo.svg';
+import Logo from '../../assets/logo_white.svg';
 
 const FooterContainer = styled(Box)<BoxProps>(({ theme }) => ({
   padding: theme.spacing(3),
-  paddingTop: theme.spacing(8),
-  backgroundColor: theme.palette.primary.light,
-  color: theme.palette.primary.main,
-  borderTop: '1px solid',
-  borderColor: theme.palette.primary.main,
+  paddingTop: theme.spacing(0),
+  backgroundColor: theme.palette.secondary.main,
+  color: theme.palette.primary.contrastText,
 }));
 
 const FooterLogo = styled('img')(() => ({
@@ -21,14 +19,28 @@ const FooterLogo = styled('img')(() => ({
   display: 'inline-block',
 }));
 
+const FooterLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  textDecoration: 'none',
+}));
+
 const socialMediaLinks = {
-  twitter: 'https://twitter.com/bando_cool_',
+  twitter: 'https://twitter.com/BandoCool',
 };
 
 export default function Footer() {
   return (
     <FooterContainer>
-      <Container maxWidth={false} sx={{ maxWidth: '1200px', minHeight: '200px' }}>
+      <Container
+        maxWidth={false}
+        sx={{
+          maxWidth: '1200px',
+          minHeight: '200px',
+          borderTop: '1px solid',
+          borderColor: 'primary.contrastText',
+          paddingTop: '30px',
+        }}
+      >
         <Grid container spacing={2} justifyContent="space-between">
           <Grid item xs={12} sm={6} md={3}>
             <FooterLogo src={Logo} loading="lazy" alt="" aria-label="Bando footer logo" />
@@ -36,45 +48,40 @@ export default function Footer() {
           <Grid item xs={6} sm={3} md={2}>
             <Typography
               variant="subtitle1"
-              sx={{ color: 'primary.main', fontWeight: 700 }}
+              sx={{ color: 'primary.contrastText', fontWeight: 700 }}
               gutterBottom
             >
               PRODUCTO
             </Typography>
-            <Link href="/faq" sx={{ color: 'ink.i900' }} display="block">
+            <FooterLink href="/faq" display="block">
               Preguntas Frecuentes
-            </Link>
-            <Link
-              sx={{ color: 'ink.i900' }}
-              href="https://academy.bando.cool"
-              target="_blank"
-              display="block"
-            >
+            </FooterLink>
+            <FooterLink href="https://academy.bando.cool" target="_blank" display="block">
               Bando Academy
-            </Link>
+            </FooterLink>
           </Grid>
           <Grid item xs={6} sm={3} md={2}>
             <Typography
               variant="subtitle1"
-              sx={{ color: 'primary.main', fontWeight: 700 }}
+              sx={{ color: 'primary.contrastText', fontWeight: 700 }}
               gutterBottom
             >
               NOSOTROS
             </Typography>
-            <Link href="/terms" sx={{ color: 'ink.i900' }} display="block">
+            <FooterLink href="/terms" display="block">
               Política de Privacidad
-            </Link>
-            <Link href="/terms" sx={{ color: 'ink.i900' }} display="block">
+            </FooterLink>
+            <FooterLink href="/terms" display="block">
               Términos y Condiciones
-            </Link>
-            <Link href="mailto:hola@bando.cool" sx={{ color: 'ink.i900' }} display="block">
+            </FooterLink>
+            <FooterLink href="mailto:hola@bando.cool" display="block">
               Contacto
-            </Link>
+            </FooterLink>
           </Grid>
           <Grid item xs={6} sm={3} md={2}>
             <Typography
               variant="subtitle1"
-              sx={{ color: 'primary.main', fontWeight: 700 }}
+              sx={{ color: 'primary.contrastText', fontWeight: 700 }}
               gutterBottom
             >
               SIGUENOS
@@ -84,12 +91,13 @@ export default function Footer() {
               target="_blank"
               component="a"
               href={socialMediaLinks.twitter}
+              sx={{ color: 'primary.contrastText' }}
             >
               <XIcon color="inherit" />
             </IconButton>
           </Grid>
         </Grid>
-        <Typography variant="body2" sx={{ color: 'ink.i900', pt: 10 }} align="center">
+        <Typography variant="body2" sx={{ color: 'primary.contrastText', pt: 10 }} align="center">
           © {new Date().getFullYear()} Bando. Todos los derechos reservados.
         </Typography>
       </Container>
