bce7ccc6b0fcedff9ab69b0a22cb475320f3d7f0
luisgj <guayabas@hey.com>
2024-02-14 10:25:30 -0600

    config file

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ import '@config/axios';
 import '@config/sentry';
 import './index.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
