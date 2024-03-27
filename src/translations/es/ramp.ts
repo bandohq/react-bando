13eff4417dad57c69b6fb49d829b85767193db6e
g6s <luisgj@users.noreply.github.com>
2024-03-26 23:29:04 -0600

    Update src/translations/es/ramp.ts

--- src/translations/es/ramp.ts
+++ src/translations/es/ramp.ts
@@ -10,6 +10,6 @@ export default {
       'Bando está en beta privado. Para poder ser de nuestros primeros usuarios envía un correo a hola@bando.cool',
     txn: 'Hubo un error al procesar tu transacción. Por favor intenta de nuevo.',
     limit: 'Has alcanzado el límite de transacciones permitidas. Por favor intenta más tarde.',
-    accountNames: 'El nombre de la cuenta no coincide con el nombre.',
+    accountNames: 'El nombre de la cuenta no coincide con tu nombre registrado.',
   },
 };
