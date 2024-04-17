afe87bedd58a5b606d5eeba22e5a1c94df916b05
g6s <guayabas@hey.com>
2024-04-17 13:39:52 -0600

    celanup

--- src/components/forms/GetQuoteForm/schema.ts
+++ src/components/forms/GetQuoteForm/schema.ts
@@ -13,21 +13,23 @@ const schema = yup.object().shape({
   operationType: yup.string().oneOf(['deposit', 'withdraw']).required(),
   baseAmount: yup.number().when(['operationType'], {
     is: 'deposit',
-    then: (schema) => 
-      schema.test(
-        'is-valid-amount',
-        'El monto mínimo es de $20 MXN',
-        (value) => value !== undefined && value >= 20,
-      )
-      .required(),
-    otherwise: (schema) => 
-      schema.test(
-        'is-valid-amount',
-        'El monto mínimo es de $2 USD',
-        (value) => value !== undefined && value >= 2,
-      )
-      .required(),
-    }),
+    then: (schema) =>
+      schema
+        .test(
+          'is-valid-amount',
+          'El monto mínimo es de $20 MXN',
+          (value) => value !== undefined && value >= 20,
+        )
+        .required(),
+    otherwise: (schema) =>
+      schema
+        .test(
+          'is-valid-amount',
+          'El monto mínimo es de $2 USD',
+          (value) => value !== undefined && value >= 2,
+        )
+        .required(),
+  }),
   quoteCurrency: yup.string().required(),
   baseCurrency: yup.string().required(),
   network: yup.string().required(),
