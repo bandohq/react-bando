cc3851312419c82edc7a67d0ceef80d4c1e3ef02
Sarai Leon <sleon90@gmail.com>
2024-06-17 10:11:59 -0600

    chore: Put intercom app id in a env var

--- src/hooks/useUser/IntercomProvider.tsx
+++ src/hooks/useUser/IntercomProvider.tsx
@@ -1,5 +1,6 @@
 import Intercom from '@intercom/messenger-js-sdk';
 import { PropsWithChildren, useEffect } from 'react';
+import env from '@config/env';
 import { User } from './types';
 
 type IntercomProviderProps = PropsWithChildren & {
@@ -9,16 +10,16 @@ type IntercomProviderProps = PropsWithChildren & {
 export default function IntercomProvider({ children, user }: IntercomProviderProps) {
   // NOTE: Boot Intercom on app start
   useEffect(() => {
-    Intercom({ app_id: 'gtz8x9mh' });
+    Intercom({ app_id: env.intercomAppId });
   }, []);
 
   // NOTE: Set or unset User data on Intercom
   useEffect(() => {
     if (!user) {
-      Intercom({ app_id: 'gtz8x9mh', email: '', name: '', id: '' });
+      Intercom({ app_id: env.intercomAppId, email: '', name: '', id: '' });
     } else {
       Intercom({
-        app_id: 'gtz8x9mh',
+        app_id: env.intercomAppId,
         ...(user.email && { email: user.email }),
         ...(user.id && { user_id: String(user.id) }),
         ...(user.firstName && { name: [user?.firstName, user.lastName].join(' ') }),
