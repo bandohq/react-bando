b892cd946b5dc1096ae351b0ee931101c3e32b65
g6s <guayabas@hey.com>
2024-06-26 17:31:54 -0600

    enable bsc

--- src/config/constants/networks.ts
+++ src/config/constants/networks.ts
@@ -208,17 +208,12 @@ const networkOptions: NetworkOption = {
     label: 'Binance Smart Chain',
     value: 'bsc',
     img: Binance,
-    enabled: false,
+    enabled: true,
     chains: [
       {
         label: 'BNB',
         value: 'BNB',
         img: BNB,
-      },
-      {
-        label: 'WETH',
-        value: 'WETH',
-        img: WETH,
       },
       {
         label: 'USDC',
