5c7cfb439d046cff0045f77f957fe298861e680c
Sarai Leon <sleon90@gmail.com>
2024-07-09 06:59:19 -0600

    use absolute values in usage

--- src/hooks/useUser/requests.ts
+++ src/hooks/useUser/requests.ts
@@ -16,8 +16,8 @@ export const getUserData: GetUserDataRequest = (endpoint) =>
       phone: data.phone,
       nationalIdNumber: data.national_id_number,
       email: data.email,
-      currentDepositUsageUsd: parseFloat(data.current_deposit_usage_usd),
-      currentWithdrawalUsageUsd: parseFloat(data.current_withdrawal_usage_usd),
+      currentDepositUsageUsd: Math.abs(parseFloat(data.current_deposit_usage_usd)),
+      currentWithdrawalUsageUsd: Math.abs(parseFloat(data.current_withdrawal_usage_usd)),
       resetAt: data.reset_at,
     }))
     .catch((error) => {
