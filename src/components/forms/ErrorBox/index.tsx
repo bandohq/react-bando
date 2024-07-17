c0986e383f5768e882778508405062879c1a4bbf
g6s <guayabas@hey.com>
2024-07-16 19:36:24 -0600

    add new limit alert with instructions

--- src/components/forms/ErrorBox/index.tsx
+++ src/components/forms/ErrorBox/index.tsx
@@ -1,17 +1,28 @@
 import Box, { BoxProps } from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 
-const ErrorBox = styled(Box)<BoxProps>(({ theme }) => ({
-  padding: theme.spacing(2),
-  marginTop: theme.spacing(2),
-  display: 'flex',
-  flexDirection: 'column',
-  gap: theme.spacing(2),
-  borderRadius: '6px',
-  color: theme.palette.error.main,
-  backgroundColor: theme.palette.error.contrastText,
-  border: `1px solid ${theme.palette.error.main}`,
-  textAlign: 'center',
-}));
+type ErrorBoxProps = BoxProps & {
+  mode?: 'error' | 'alert';
+  align?: 'center' | 'left' | 'right';
+};
+
+const ErrorBox = styled(Box)<ErrorBoxProps>(({ theme, mode, align }) => {
+  if (align) align = align === 'center' ? 'center' : align === 'left' ? 'left' : 'right';
+  else align = 'center';
+  let color = theme.palette.error.main;
+  if (mode) color = mode === 'error' ? theme.palette.error.main : theme.palette.info.main;
+  return {
+    padding: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+    borderRadius: '6px',
+    color: color,
+    backgroundColor: theme.palette.error.contrastText,
+    border: `1px solid ${color}`,
+    textAlign: align,
+  };
+});
 
 export default ErrorBox;
--- src/translations/es/ramp.ts
+++ src/translations/es/ramp.ts
@@ -9,8 +9,21 @@ export default {
     forbidden:
       'Bando está en beta privado. Para poder ser de nuestros primeros usuarios envía un correo a soporte@bando.cool',
     txn: 'Hubo un error al procesar tu transacción. Por favor intenta de nuevo.',
-    limit:
-      'Has alcanzado tu límite de 500 USD. Para poder subir tu limite a 10,000 USD envía un correo a soporte@bando.cool',
+    limit: `
+      <h4>¡Has alcanzado tu límite mensual!</h4>
+      <strong>¿Cómo hago para aumentar mi límite en Bando?</strong>
+      </p>
+        Con el nivel inicial, el límite de compra es de hasta $500 USD mensuales.
+        Para aumentar tu límite hasta $9,999 USD al mes necesitas:
+      </p>
+      <ol>
+        <li>Abre el chat de soporte de bando y solicita un aumento.</li>
+        <li>Envía foto de tu INE (Frente y vuelta) o pasaporte</li>
+        <li>Envía tu Comprobante de Domicilio</li>
+        <li>Envía la dirección de tu billetera con la enviarás activos a Bando</li>
+      </ol>
+      <h6>Una vez con tus documentos, la verificación puede tardar hasta 24 horas.</h6>
+    `,
     accountNames: 'El nombre de la cuenta no coincide con tu nombre registrado.',
   },
 };
