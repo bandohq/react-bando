4cabc2e307d1a752fbc026064a77891607bcb8ad
g6s <guayabas@hey.com>
2024-08-07 15:44:24 -0600

    add quote translation for eng.

---
+++ src/translations/en/quote.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'Welcome to crypto.',
+  selectNetwork: 'Select chain and token',
+  on: 'You pay',
+  off: 'You get',
+  recipient: 'You get',
+  noData: 'No tokens found.',
+  notFoundText: `
+    Reasons for that could be: low liquidity, amount selected is too low, gas costs are too high or there are no routes for the selected combination.
+  `,
+  notFoundTitle: 'No routes available',
+};
